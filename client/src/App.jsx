359d6d54549c6507dc138af7c4b55e2f4d3a55c5
Mushfiqur Niaz <mushfiqurniaz33@gmail.com>
2024-08-10 19:26:46 +0600

    declared routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
+import { Toaster } from "sonner";
+import AddUserPage from "./pages/AddUserPage.jsx";
 import SearchPage from "./pages/SearchPage.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <SearchPage />
+      <BrowserRouter>
+        <Toaster duration={2000} position="top-center" richColors closeButton />
+        <Routes>
+          <Route path="/" element={<SearchPage />} />
+          <Route path="/adduser" element={<AddUserPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
